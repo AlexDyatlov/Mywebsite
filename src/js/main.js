cc673db1f6727c5abc942674f80bcdef01272bde
AlexDyatlov <aleks78.gooogle@gmail.com>
2020-07-05 00:10:34 +0300

    added adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -25,6 +25,35 @@ $(function(){
     
   }
 
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
 
 });
+
+if(window.matchMedia('(max-width: 1000px)').matches){
+
+  document.querySelectorAll('.skills__col-title').forEach((item) =>
+  item.addEventListener('click', () => {
+
+    const parent = item.parentNode;
+    
+        if (parent.classList.contains('skills__col--active')) {
+            parent.classList.remove('skills__col--active');
+        } 
+        else {
+        
+          document
+            .querySelectorAll('.skills__col')
+            .forEach((child) => child.classList.remove('skills__col--active'))
+              
+          parent.classList.add('skills__col--active');
+          
+        }
+    
+    })
+  )
+
+}
+
 
